61efb6e86d48e600e834f07db38c195b87fc5940
Your Name <you@example.com>
2022-11-25 12:30:04 +0100

    checking for winner

--- scripts/game.js
+++ scripts/game.js
@@ -18,11 +18,61 @@ function switchPlayer() { //cambia al otro jugador
 }
 
 function selectGameField(event) {
-    // if (event.target.tagName !== 'LI') { //si el click NO es en un LI, el resto de la funcion no sigue
-    //     return;
-    // }
+   
+    const selectedField = event.target;    
+    
+    const selectedColumn = selectedField.dataset.col - 1; //para convertirlo a num(es string)
+    const selectedRow = selectedField.dataset.row - 1;
 
-    event.target.textContent = players[activePlayer].symbol; // = players[0] (marca su simbolo en el field)
-    event.target.classList.add('disabled'); 
+    if (gameData[selectedRow][selectedColumn] > 0) {
+        alert('Duh!!');
+        return;
+    }
+
+    selectedField.textContent = players[activePlayer].symbol; // = players[0] (marca su simbolo en el field)
+    selectedField.classList.add('disabled');
+
+    gameData[selectedRow][selectedColumn] = activePlayer + 1; // se suma 1 para que devuelva 1 o 2 (en vez de 0 o 1)
+    console.log(gameData);
+
+    const winnerId = checkForGameOver();
+    console.log(winnerId);
+
+    currentRound++;
     switchPlayer();
+}
+
+function checkForGameOver() {
+    //Checking the ROWS for equality (winner)
+    for (let i = 0; i < 3; i++){
+        if(gameData[i][0] > 0 && gameData[i][0] === gameData[i][1] && gameData[0][1] === gameData[0][2]) {
+            return gameData[i][0];
+        }
+    }
+
+    //Checking for COLUMNS
+    for (let i = 0; i < 3; i++){
+        if(gameData[0][i] > 0 && gameData[0][i] === gameData[1][i] && gameData[2][i] === gameData[0][2]) {
+            return gameData[0][i];
+        }
+    }
+
+    //Checking DIAGONAL: top left to bottom right
+    if (
+        gameData[0][0] > 0 && gameData[0][0] === gameData[1][1] && gameData[1][1] === gameData[2][2]
+    ) {
+        return gameData[0][0];
+    }
+    //Checking DIAGONAL: bottom left to top right
+    if (
+        gameData[2][0] > 0 && gameData[2][0] === gameData[1][1] && gameData[1][1] === gameData[0][2]
+    ) {
+        return gameData[2][0];
+    }
+      
+    //Checking for a DRAW, after the 9th round
+    if (currentRound === 9){
+        return -1;
+    }
+    return 0;
 }
